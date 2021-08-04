ac941ab0fae989c8d7d934d0ed5ec8b77260d3d1
dmilagrosv <87400389+dmilagrosv@users.noreply.github.com>
2021-08-04 17:12:18 -0500

    profile - prifileSteps

--- src/components/CardShadow/CardShadow.js
+++ src/components/CardShadow/CardShadow.js
@@ -6,7 +6,7 @@ const CardShadow = ({ title, paragraph, content }) => {
   return (
     <div className="card-shadow">
       <h6>{title}</h6>
-      <p>{paragraph}</p>
+      <p className="card-shadow-paragraph">{paragraph}</p>
       {content}
     </div>
   )
--- src/pages/store/profile/ProfileStep3.js
+++ src/pages/store/profile/ProfileStep3.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
+import ButtonFilled from '../../../components/Button/ButtonFilled'
 import CardShadow from '../../../components/CardShadow/CardShadow'
+import DragnDrop from '../../../components/DragnDrop/DragnDrop'
+import previewImage from '../../../images/producto/preview.svg';
 
 import './Profile.css'
 
 const ProfileStep3 = () => {
   const [fileName, setFileName] = useState('')
+  const [selectedFile, setSelectedFile] = useState("");
 
   const handleFileChange = (event) => {
     event.preventDefault();
     let input = event.target;
-    console.log(event.target.files[0])
     if (input.files && input.files[0]) {
       const sizeByte = input.files[0].size;
       const sizeMegaByte = parseFloat(sizeByte / (1024 * 1024)).toFixed(2);
@@ -17,33 +20,33 @@ const ProfileStep3 = () => {
       reader.onload = (e) => {
         if (sizeMegaByte > 1) {
           console.log("El tamaño máximo es de 1mb")
-          setFileName(input.files[0].name)
         } else {
-          setFileName(input.files[0].name)
+          // setAlertError(null)
+          setSelectedFile({
+            ...selectedFile,
+            [input.name]: input.files[0],
+            // imgOfertaPantalla: e.target.result,
+          });
         }
+
       };
       reader.readAsDataURL(input.files[0]);
 
     }
   };
+
   return (
     <div>
       <CardShadow
-        title="1.- Adjunta un logo para tu tienda"
-        paragraph="Subir un logo correctamente fortalece la imagen y seriedad del negocio, de esta manera, juntos podremos darles a nuestros clientes la confianza de comprar con nosotros."
+        title="3.- Adjunta banners promocionales"
+        paragraph="Es importante entender que este es el contacto más importante que tenemos con el consumidor final. Si tenemos un diseño con una buena calidad de imagen y una diagramación atractiva para nuestro público, es mucho más probable de que se concrete una compra."
         content={
-          <div className="profile-store-content-card">
-            <p>Debe tener como medidas 446x168 px y una resolución de 300px</p>
-            <div className="profile-store-content-input">
-              <div className="profile-store-container-input">
-                <p className="profile-store-filename">{fileName} </p>
-              </div>
-              <button>
-                Cargar Imagen
-                <input
-                  type="file"
-                  accept="image/*" onChange={handleFileChange} />
-              </button>
+          <div className="profile-store-content-card" >
+            <p>Debe tener como medidas 1111x352 px y una resolución de 300px:</p>
+            <div className="profile-store-content-dragndrop">
+              <DragnDrop image={previewImage} name="imagenProducto1" id="imagenProducto1" onChange={handleFileChange} required='true' />
+              <DragnDrop image={previewImage} name="imagenProducto1" id="imagenProducto1" onChange={handleFileChange} required='true' />
+              <DragnDrop image={previewImage} name="imagenProducto1" id="imagenProducto1" onChange={handleFileChange} required='true' />
             </div>
           </div>
         }
