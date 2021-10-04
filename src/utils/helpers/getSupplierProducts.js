b6cd4785b637b3349bf1cc19ccd32c5671e67424
zFranco23 <francoheflo@gmail.com>
2021-10-04 11:40:07 -0500

    changes conflict

--- src/utils/helpers/getSupplierProducts.js
+++ src/utils/helpers/getSupplierProducts.js
@@ -5,7 +5,7 @@ export const getSupplierProducts = async ( token ) => {
         const userType = "admin";
         const idSupplier = "614cdeb4686d50c2139605be";
 
-        const { data }  = await axios.get(`${process.env.REACT_APP_BACKEND_URL_BUSINESS}/get-product-pagination/${userType}?skip=0&limit=10&state=A&supplier=${idSupplier}`);
+        const { data }  = await axios.get(`${process.env.REACT_APP_BACKEND_URL_BUSINESS}/get-product-pagination/${userType}?skip=0&limit=10&supplier=${idSupplier}`);
         
         if(data?.productosGeneral){
             const cleanData = prepareDataProductSupplier( data.productosGeneral );
