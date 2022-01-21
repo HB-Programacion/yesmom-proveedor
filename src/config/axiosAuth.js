038e39ef60015088f3f67724f62b36a232eb098c
zFranco23 <francoheflo@gmail.com>
2022-01-21 10:55:43 -0500

    aaa

--- src/config/axiosAuth.js
+++ src/config/axiosAuth.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-console.log('CLIENTE AUTTTHH',process.env.REACT_APP_BACKEND_URL_SECURITY);
 const clienteAxiosAuth = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL_SECURITY
 })
